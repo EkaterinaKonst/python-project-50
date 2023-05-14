64b8a77e2cc91c5e4ba9189b6652bccbc5be2723
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-05-14 15:53:08 +0300

    4 step

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -4,7 +4,18 @@ import pytest
 from tests import get_path
 
 
-def test_generare_diff(test_input1, test_input2, expected):
+@pytest.mark.parametrize(
+    "test_input1,test_input2, expected",
+    [
+        pytest.param(
+            'file1.json',
+            'file2.json',
+            'correct_result.txt',
+            id="flat_json_file"
+        ),
+    ],
+)
+def test_generate_diff(test_input1, test_input2, expected):
     expected_path = get_path(expected)
     with open(expected_path, 'r') as file:
         result_data = file.read()
