e0a7ab6eaf05731456b61ae4b8dbc0c4fbb00eca
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-06-13 21:05:03 +0300

    6 step

--- gendiff/make_parser.py
+++ gendiff/make_parser.py
@@ -8,5 +8,5 @@ def parser_func():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format')
+    parser.add_argument('-f', '--format', default='stylish')
     return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,10 @@
 from gendiff.make_parser import parser_func
-from gendiff.some_code import generate_diff
+from gendiff.some_code import stylish
 
 
 def main():
     args = parser_func()
-    print(generate_diff(args.first_file, args.second_file))
+    print(stylish(args.first_file, args.second_file, formatter='stylish'))
 
 
 if __name__ == '__main__':
