a6fe2466d168d257ccb043735ca5bda1fafd912b
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-06-18 01:16:48 +0300

    almost 7 step

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,4 +1,4 @@
-from gendiff.some_code import stylish
+from gendiff.some_code import *
 
 
 fl1 = 'tests/fixtures/file1.json'
@@ -16,12 +16,12 @@ stylish_nested_result = open('tests/fixtures/correct_result_tree.txt', 'r')
 
 
 def test_gendiff_flat_json():
-    assert stylish(fl1, fl2) == read_result1.read()
+    assert generate_diff(fl1, fl2) == read_result1.read()
 
 
 def test_gendiff_flat_yaml():
-    assert stylish(fl1_yaml, fl2_yaml) == yaml_result.read()
+    assert generate_diff(fl1_yaml, fl2_yaml) == yaml_result.read()
 
 
 def test_stylish_nested():
-    assert stylish(stylish_nested1, stylish_nested2) == stylish_nested_result.read()
+    assert generate_diff(stylish_nested1, stylish_nested2) == stylish_nested_result.read()
