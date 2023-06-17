692db1669714902f8b83dabe3aebab65a2fc6b9c
Ekaterina Konstantinova <feldsher2812@gmail.com>
2023-06-18 00:12:11 +0300

    alm 6.5 st

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,51 +1,27 @@
-from gendiff.some_code import stylish
-import pytest
-import os
+from gendiff.generate_diff import generate_diff
 
 
-def get_path(file):
-    return os.path.join('tests', 'fixtures', file)
+fl1 = 'tests/fixtures/file1.json'
+fl2 = 'tests/fixtures/file2.json'
+result1 = 'tests/fixtures/correct_result.txt'
+read_result1 = open(result1, 'r')
+
+fl1_yaml = 'tests/fixtures/file1.yml'
+fl2_yaml = 'tests/fixtures/file2.yml'
+yaml_result = open('tests/fixtures/correct_result.txt', 'r')
+
+stylish_nested1 = 'tests/fixtures/file1nested.json'
+stylish_nested2 = 'tests/fixtures/file2nested.json'
+stylish_nested_result = open('tests/fixtures/correct_result_tree.txt', 'r')
 
 
-@pytest.mark.parametrize(
-    "test_input1,test_input2, formater,  expected",
-    [
-        pytest.param(
-            'file1.json',
-            'file2.json',
-            'stylish',
-            'correct_result.txt',
-            id="flat_json_file"
-        ),
-        pytest.param(
-            'file1.yml',
-            'file2.yml',
-            'stylish',
-            'correct_result.txt',
-            id="flat_yaml_file"
-        ),
-        pytest.param(
-            'file1tree.json',
-            'file2tree.json',
-            'stylish',
-            'correct_result_tree.txt',
-            id="tree_json_file"
-        ),
-        pytest.param(
-            'file1tree.yml',
-            'file2tree.yml',
-            'stylish',
-            'correct_result_tree.txt',
-            id="tree_yaml_file"
-        ),
-    ],
-)
+def test_gendiff_flat_json():
+    assert stylish(fl1, fl2) == read_result1.read()
 
 
-def test_generare_diff(test_input1, test_input2, formater, expected):
-    expected_path = get_path(expected)
-    with open(expected_path, 'r') as file:
-        result_data = file.read()
-        test_path1 = get_path(test_input1)
-        test_path2 = get_path(test_input2)
-        assert stylish(test_path1, test_path2, formater) == result_data
+def test_gendiff_flat_yaml():
+    assert stylish(fl1_yaml, fl2_yaml) == yaml_result.read()
+
+
+def test_stylish_nested():
+    assert stylish(stylish_nested1, stylish_nested2) == stylish_nested_result.read()
